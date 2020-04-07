81c82c546aa5a66188c7ce530222d6226bcdd76b
raffael-grasberger <raffael.grasberger@gmail.com>
2020-04-07 09:03:07 +0200

    Added functions for creating special matrix and returning an inverted matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions for creating a special matrix that can store an inverted matrix,
+## and for getting the inverted matrix as a result
 
-## Write a short comment describing this function
+## Creates a special matrix, which is really a list containing function to
+## set/get the value of the matrix, set/get the value of the cached inverted matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    c <- NULL
+    set <- function(y) {
+        x <<- y
+        c <<- NULL
+    }
+    get <- function() x
+    setcache <- function(cache) c <<- cache
+    getcache <- function() c
+    list(set = set, get = get,
+         setcache = setcache,
+         getcache = getcache)
 }
 
 
-## Write a short comment describing this function
+## Returns an inversed matrix (takes the cached matrix if already computed)
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    c <- x$getcache()
+    if(!is.null(c)) {
+        message("getting cached data")
+        return(c)
+    }
+    data <- x$get()
+    c <- solve(data, ...)
+    x$setcache(c)
+    c
 }
